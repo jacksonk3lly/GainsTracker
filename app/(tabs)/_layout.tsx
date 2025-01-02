31c0c00910033f0761dd78942d2cc07d85506d35
Jackson Kelly <jacksonshane04@gmail.com>
2025-01-02 18:57:16 +1300

    added Resume Button Overlay

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,82 +1,134 @@
-import { Stack } from "expo-router";
+import { Stack, useFocusEffect, useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { View, Text, StyleSheet, Alert, Button } from "react-native";
+import React, { useCallback, useState } from "react";
+import { activeWorkoutExists } from "@/db";
 
 export default function TabLayout() {
+  const router = useRouter();
   const [fontsLoaded] = useFonts({
     "SedgwickAveDisplay-Regular": require("../../assets/fonts/SedgwickAveDisplay-Regular.ttf"),
   });
 
-  if (!fontsLoaded) {
-    return null; // or a loading spinner
+  // if (!fontsLoaded) {
+  //   return null; // or a loading spinner
+  // }
+
+  function resumeWorkout() {
+    if (activeWorkoutExists()) {
+      router.navigate("/activeWorkout");
+    } else {
+      Alert.alert("No active workout to resume");
+    }
   }
 
+  const [resumeButtonShown, setResumeButtonShown] = useState(
+    activeWorkoutExists()
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      setResumeButtonShown(activeWorkoutExists());
+    }, [])
+  );
+
   return (
-    <Tabs
-      screenOptions={{
-        // tabBarActiveTintColor: "#ffd33d",
-        headerStyle: {
-          backgroundColor: "green",
-        },
-        headerShown: true,
-        headerTintColor: "#fff",
-        headerShadowVisible: false,
-        tabBarStyle: {
-          backgroundColor: "#25292e",
-        },
-        tabBarActiveTintColor: "green",
-        headerTitleStyle: {
-          fontFamily: fontsLoaded ? "SedgwickAveDisplay-Regular" : "System",
-          fontSize: 30, // Add your desired font size here
-          // color: "green",
-          // height: 100,
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Previous Workouts",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
+    <>
+      <Tabs
+        screenOptions={{
+          // tabBarActiveTintColor: "#ffd33d",
+          headerStyle: {
+            backgroundColor: "green",
+          },
+          headerShown: true,
+          headerTintColor: "#fff",
+          headerShadowVisible: false,
+          tabBarStyle: {
+            backgroundColor: "#25292e",
+          },
+          tabBarActiveTintColor: "green",
+          headerTitleStyle: {
+            fontFamily: fontsLoaded ? "SedgwickAveDisplay-Regular" : "System",
+            fontSize: 30, // Add your desired font size here
+            // color: "green",
+            // height: 100,
+          },
         }}
-      />
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Previous Workouts",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="create"
-        options={{
-          title: "Create Workouts",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create Workouts",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={focused ? "home-sharp" : "home-outline"}
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "DEV",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? "information-circle" : "information-circle-outline"
-              }
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "about",
+            tabBarIcon: ({ color, focused }) => (
+              <Ionicons
+                name={
+                  focused ? "information-circle" : "information-circle-outline"
+                }
+                color={color}
+                size={24}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+      {resumeButtonShown && (
+        <View style={styles.floatingButton}>
+          <Button
+            title="Resume Workout"
+            color="#fff"
+            onPress={resumeWorkout}
+          ></Button>
+        </View>
+      )}
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: "absolute",
+    bottom: 100, // Adjust based on tab height
+    right: 20,
+    left: 20,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+    borderRadius: 50,
+    // padding: 15,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
